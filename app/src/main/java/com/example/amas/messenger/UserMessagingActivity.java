8515109671c4d0f10cfe2142bb5b454a13a4ecc7
Amirhossein Hediehloo <amirhosseinhediehloo@gmail.com>
2018-08-17 14:33:58 +0430

    from_message layout created. MessageTo and MessageFrom inner classes created

--- app/src/main/java/com/example/amas/messenger/UserMessagingActivity.java
+++ app/src/main/java/com/example/amas/messenger/UserMessagingActivity.java
@@ -1,32 +1,16 @@
 package com.example.amas.messenger;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.ImageView;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
-import com.xwray.groupie.GroupAdapter;
 import com.xwray.groupie.Item;
 import com.xwray.groupie.ViewHolder;
-
-import java.util.Iterator;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -61,11 +45,22 @@ public class UserMessagingActivity extends AppCompatActivity implements View.OnC
         int id = v.getId();
     }
 
-    class MessageItem extends Item<ViewHolder> {
+    class MessageToItem extends Item<ViewHolder> {
         @Override
         public void bind(@NonNull ViewHolder viewHolder, int position) {
             if(user != null)
-            Picasso.get().load(user.profilePhotoUrl).into((CircleImageView) viewHolder.itemView.findViewById(R.id.profile_image_messaging_activity));
+            Picasso.get().load(user.profilePhotoUrl).into((CircleImageView) viewHolder.itemView.findViewById(R.id.from_message_profile_image));
+        }
+        @Override
+        public int getLayout() {
+            return R.layout.to_chat_message;
+        }
+    }
+    class MessageFromItem extends Item<ViewHolder> {
+        @Override
+        public void bind(@NonNull ViewHolder viewHolder, int position) {
+            if(user != null)
+            Picasso.get().load(user.profilePhotoUrl).into((CircleImageView) viewHolder.itemView.findViewById(R.id.from_message_profile_image));
         }
         @Override
         public int getLayout() {
